7e1752c8a5acabca1e5df4c606e888a03fe01a23
JayanthGanta45 <gantajayanth45@gmail.com>
2023-07-31 22:21:03 +0530

    Programming Marathon -1
    Simple programming from basics

---
+++ TheCubeMystery/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter a Number");
+        int num=scan.nextInt();
+        System.out.println(cubeNumber(num));
+    }
+    public static int cubeNumber(int num)
+    {
+        return num*num*num;
+    }
+}
